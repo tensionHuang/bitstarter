398af2f68ec6151cf72d1d684c050d75b20ef27d
tensenHuang <yuyuhunag0711@gmail.com>
2013-07-09 21:14:03 +0800

    web.js read index.html

--- web.js
+++ web.js
@@ -3,7 +3,8 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+	var page = fs.readFileSync('index.html','utf-8');
+	response.send(page);
 });
 
 var port = process.env.PORT || 5000;
